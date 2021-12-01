832733bd9a1a6807277ca96bfa05a4db093b8de9
taichi <taicloud0803@icloud.com>
2021-12-02 00:57:05 +0900

    Initial Commit

---
+++ AlamofirePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofirePractice
+//
+//  Created by taichi on 2021/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
